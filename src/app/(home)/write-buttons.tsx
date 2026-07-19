635d9e71817760b89bdd112f1de1da41180ca18d
Elana Liao <xuyi7126@gmail.com>
2026-07-19 15:09:37 +1000

    color tuning

--- src/app/(home)/write-buttons.tsx
+++ src/app/(home)/write-buttons.tsx
@@ -63,7 +63,7 @@ export default function WriteButton() {
 					whileTap={{ scale: 0.95 }}
 					onClick={() => setConfigDialogOpen(true)}
 					className='p-2'>
-					<DotsSVG className='h-6 w-6' />
+					<DotsSVG className='text-brand h-6 w-6' />
 				</motion.button>
 			</motion.div>
 		</HomeDraggableLayer>
